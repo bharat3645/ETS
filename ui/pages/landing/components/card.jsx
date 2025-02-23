ea5244682132256d5e664dca8fae05fcd88f5027
Anmol Srivastava <a4anmol93.6@gmail.com>
2025-02-23 20:17:46 +0530

    fix

--- ui/pages/landing/components/card.jsx
+++ ui/pages/landing/components/card.jsx
@@ -19,8 +19,8 @@ const card = (type,title,desc,reg,date,i) => {
                     <span>{reg}+ Registrations</span>
                 </div>
                 <div className="book grid grid-cols-[50%_50%]">
-                    <div className="flex">
-                        <div className="w-[70%] text-center p-[10px] bgl text-white font-medium"><a class="!text-white" href='/home'></a></div>
+                    <div className="flex whitespace-nowrap">
+                        <div className="w-[70%] text-center p-[10px] bgl text-white font-medium"><a class="!text-white" href='/home'>Book now</a></div>
                     </div>
                     <div className="flex justify-end items-center text-xl text-(--bg-h) space-x-2">
                         {<Clock/>}
